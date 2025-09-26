7e8ec932342db7507759179a92454916bc81b1e8
Svetlana <kuzminovasveta19@gmail.com>
2025-09-26 14:58:50 +0300

    Init commit

---
+++ src/test/java/com/example/dumpdbproject/DumpDbProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dumpdbproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DumpDbProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
